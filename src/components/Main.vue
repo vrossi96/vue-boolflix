b874e2942e992d4dca04242d848a1706e4ece5d2
vrossi96 <vrossi396@yahoo.it>
2022-02-14 14:39:10 +0100

    add icons for it and eng languages

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,7 +4,14 @@
          <h1 v-if="!movies.length">CERCA UN FILM</h1>
          <ul v-else>
             <li v-for="(movie, index) in movies" :key="movie.id || index">
-               Titolo: {{ movie.title }}, Titolo originale: {{ movie.original_title }} Lingua: {{ movie.original_language }} Voto:
+               Titolo: {{ movie.title }}, Titolo originale: {{ movie.original_title }} Lingua: {{ movie.original_language }}
+               <img
+                  v-if="movie.original_language === 'it' || movie.original_language === 'en'"
+                  class="d-inline-block"
+                  :src="require(`../assets/img/${movie.original_language}.png`)"
+                  alt=""
+               />
+               Voto:
                {{ movie.vote_average }}
             </li>
          </ul>
@@ -23,5 +30,11 @@ export default {
 main {
    height: calc(100vh - 70px);
    background-color: #434343;
+
+   img {
+      height: 15px;
+      width: 25px;
+      border-radius: 2px;
+   }
 }
 </style>
