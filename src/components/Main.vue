8c82e54c9046be8d6fb85d62c395082577347bdc
vrossi96 <vrossi396@yahoo.it>
2022-02-15 10:46:42 +0100

    add hover with img in "cards"

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,32 +5,46 @@
             <h1>CERCA UN FILM</h1>
          </div>
          <div v-else>
-            <h2>FILM</h2>
-            <ul class="row">
+            <h2 class="py-3">FILM</h2>
+            <ul class="row g-3">
                <li class="col-3" v-for="(movie, index) in movies" :key="movie.id || index">
-                  Titolo: {{ movie.title }}, Titolo originale: {{ movie.original_title }}
-                  <img
-                     v-if="lang.includes(movie.original_language)"
-                     class="d-inline-block"
-                     :src="require(`../assets/img/${movie.original_language}.png`)"
-                     :alt="movie.original_language"
-                  />
-                  Voto:
-                  {{ movie.vote_average }}
+                  <img v-if="movie.poster_path" class="poster" :src="cardBg + movie.poster_path" alt="" />
+                  <img v-else class="poster" src="https://i.pinimg.com/originals/c3/c7/d5/c3c7d5d3c5090645d0bc9a44b83c2d40.jpg" alt="" />
+                  <div class="info">
+                     <p>Titolo: {{ movie.title }}</p>
+                     <p>Titolo originale: {{ movie.original_title }}</p>
+                     <p>
+                        Paese d'origine:
+                        <img
+                           v-if="lang.includes(movie.original_language)"
+                           class="d-inline-block"
+                           :src="require(`../assets/img/${movie.original_language}.png`)"
+                           :alt="movie.original_language"
+                        />
+                     </p>
+                     <p>Voto: {{ movie.vote_average }}</p>
+                  </div>
                </li>
             </ul>
-            <h2>SERIE</h2>
-            <ul class="row">
+            <h2 class="py-3">SERIE</h2>
+            <ul class="row g-3">
                <li class="col-3" v-for="(tv, index) in tvSeries" :key="tv.id || index">
-                  Titolo: {{ tv.name }}, Titolo originale: {{ tv.original_name }}
-                  <img
-                     v-if="lang.includes(tv.original_language)"
-                     class="d-inline-block"
-                     :src="require(`../assets/img/${tv.original_language}.png`)"
-                     :alt="tv.original_language"
-                  />
-                  Voto:
-                  {{ tv.vote_average }}
+                  <img v-if="tv.poster_path" class="poster" :src="cardBg + tv.poster_path" alt="" />
+                  <img v-else class="poster" src="https://i.pinimg.com/originals/c3/c7/d5/c3c7d5d3c5090645d0bc9a44b83c2d40.jpg" alt="" />
+                  <div class="info">
+                     <p>Titolo: {{ tv.name }}</p>
+                     <p>Titolo originale: {{ tv.original_name }}</p>
+                     <p>
+                        Paese d'origine:
+                        <img
+                           v-if="lang.includes(tv.original_language)"
+                           class="d-inline-block"
+                           :src="require(`../assets/img/${tv.original_language}.png`)"
+                           :alt="tv.original_language"
+                        />
+                     </p>
+                     <p>Voto: {{ tv.vote_average }}</p>
+                  </div>
                </li>
             </ul>
          </div>
@@ -57,12 +71,32 @@ main {
    background-color: #434343;
    overflow-y: scroll;
 
+   .col-3 {
+      overflow-x: hidden;
+   }
    ul {
       list-style: none;
 
       li {
          position: relative;
-         height: 88px;
+         height: 400px;
+         &:hover {
+            .info {
+               display: block;
+            }
+            .poster {
+               display: none;
+            }
+         }
+      }
+      .poster {
+         height: 100%;
+         width: 100%;
+      }
+      .info {
+         display: none;
+         background-color: rgba(0, 0, 0, 0.75);
+         height: 100%;
       }
    }
 
