f444969b952427bf4a05d374f521fd00fe3aa415
vrossi96 <vrossi396@yahoo.it>
2022-02-16 13:01:10 +0100

    done get cast for each movie and tv

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,11 @@ export default {
    data() {
       return {
          movies: [],
+         moviesId: [],
+         movieCast: [],
          tvSeries: [],
+         tvId: [],
+         tvCast: [],
          api: {
             baseUri: "https://api.themoviedb.org/3",
             apiKey: "e03c5cb8dddd1d7d20fb6adf3922071d",
@@ -28,7 +32,33 @@ export default {
          },
       };
    },
-   computed: {},
+   computed: {
+      getMoviesId() {
+         return this.movies.forEach((item) => {
+            this.moviesId.push(item.id);
+         });
+      },
+      getTvId() {
+         return this.tvSeries.forEach((item) => {
+            this.tvId.push(item.id);
+         });
+      },
+
+      getMovieCast() {
+         return this.moviesId.forEach((id) => {
+            axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=e03c5cb8dddd1d7d20fb6adf3922071d&language=it-IT`).then((res) => {
+               this.movieCast.push(res.data.cast);
+            });
+         });
+      },
+      getTvCast() {
+         return this.tvId.forEach((id) => {
+            axios.get(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=e03c5cb8dddd1d7d20fb6adf3922071d&language=it-IT`).then((res) => {
+               this.tvCast.push(res.data.cast);
+            });
+         });
+      },
+   },
    methods: {
       getResultsSearch(term) {
          if (!term) {
@@ -43,6 +73,10 @@ export default {
                query: term,
             },
          };
+         this.moviesId = [];
+         this.movieCast = [];
+         this.tvId = [];
+         this.tvCast = [];
 
          this.callApi("/search/movie", config, "movies");
          this.callApi("/search/tv", config, "tvSeries");
@@ -53,6 +87,7 @@ export default {
             this[array] = res.data.results;
          });
       },
+      // https://api.themoviedb.org/3/movie/49397/credits?api_key=e03c5cb8dddd1d7d20fb6adf3922071d&language=it-IT
    },
    mounted() {},
 };
